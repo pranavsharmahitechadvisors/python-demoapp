1a4fb61153c8b865d4502a0f1c51fe9e1acbb336
Pranav Sharma <pranav165@gmail.com>
2022-01-06 20:24:02 +0530

    port

--- src/run.py
+++ src/run.py
@@ -4,7 +4,7 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 65534))
+    port = int(os.environ.get("PORT", 8092))
     app.jinja_env.auto_reload = True
     app.config["TEMPLATES_AUTO_RELOAD"] = True
     app.run(host="0.0.0.0", port=port)
