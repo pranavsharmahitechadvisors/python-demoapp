d0aaa5606175773ebbb758337d7dcd9e3d56c3bd
Pranav Sharma <pranav165@gmail.com>
2022-01-14 17:08:53 +0530

    test

--- tests/tests-e2e/e2e.py
+++ tests/tests-e2e/e2e.py
@@ -2,7 +2,7 @@ import requests
 
 if __name__ == "__main__":
     print("Running E2E test to check the processes information")
-    r = requests.get("http://localhost:5000/info", headers={'Content-Type': 'application/json'})
+    r = requests.get("http://127.0.0.1:5000/info", headers={'Content-Type': 'application/json'})
     assert r.status_code == 200
     assert "Hostname" in r.text
     print("E2E Tests passed")
